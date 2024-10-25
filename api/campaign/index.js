0dc0e86e32f748902881bab4d16f49f28ab1174e
Usman209 <usmanjamil196@gmail.com>
2024-10-25 05:23:59 +0500

    auth

--- api/campaign/index.js
+++ api/campaign/index.js
@@ -2,24 +2,30 @@ const express = require('express');
 const router = express.Router();
 const campaignController = require('./controller'); // Adjust the path as needed
 
+const { authenticateAndAuthorize } = require("../../lib/utils/verifyToken");
+const { EUserRole } = require('../../lib/utils/enum');
+
+
+
+
 // Create a new campaign
-router.post('/', campaignController.createCampaign);
+router.post('/',   authenticateAndAuthorize([EUserRole.SUPER_ADMIN]), campaignController.createCampaign);
 
 // Get all campaigns
-router.get('/', campaignController.getAllCampaigns);
+router.get('/',  authenticateAndAuthorize([EUserRole.SUPER_ADMIN]), campaignController.getAllCampaigns);
 
 // Get a campaign by ID
-router.get('/:id', campaignController.getCampaignById);
+router.get('/:id',  authenticateAndAuthorize([EUserRole.SUPER_ADMIN]), campaignController.getCampaignById);
 
-router.get('/active/:id', campaignController.activateCampaign);
+router.get('/active/:id',  authenticateAndAuthorize([EUserRole.SUPER_ADMIN]), campaignController.activateCampaign);
 
-router.get('/inactive/:id', campaignController.deactivateCampaign);
+router.get('/inactive/:id',  authenticateAndAuthorize([EUserRole.SUPER_ADMIN]),campaignController.deactivateCampaign);
 
 
 
 
 // Update a campaign by ID
-router.put('/:id', campaignController.updateCampaign);
+router.put('/:id',  campaignController.updateCampaign);
 
 // Delete a campaign by ID
 router.delete('/:id', campaignController.deleteCampaign);
