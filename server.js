7e7782bee950dc1767bbb16d8d3c23ca78e54192
Usman209 <usmanjamil196@gmail.com>
2024-10-30 14:44:17 +0500

    code

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ const { bullMasterApp } = require('./api/survey/controller'); // Destructure to 
 const { HOST, PORT, SESS_SECRET } = require("./config/config");
 
 const app = express();
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+
+
 const { dbConnection } = require("./lib/utils/connection.js");
 
 // View engine setup
