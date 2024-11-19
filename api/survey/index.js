d1f381ae2d9fa25bd285cf4ccc07baee952eceeb
Usman Jamil <UJamil@lmkr.com>
2024-11-19 11:37:19 +0500

    code

--- api/survey/index.js
+++ api/survey/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 const surveyController = require('./controller'); // Adjust the path as needed
 
+
+const { 
+    dbConnection, 
+    checkDBConnection, 
+    initializeDatabase,
+    getConnectionStatus 
+} = require("../../lib/utils/connection");
+
+
 // Define routes
 router.get('/', surveyController.getCollectedData); // Create a new survey
 
 
-router.post('/', surveyController.syncCollectedData); // Create a new survey
+router.post('/', checkDBConnection, surveyController.syncCollectedData); // Create a new survey
 
 
 router.get('/count', surveyController.getCollectionCount);
