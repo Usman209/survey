a64350f57060c29220b7b36593308625b6d1d1ea
Usman Jamil <UJamil@lmkr.com>
2025-01-26 20:39:06 +0500

    code

--- api/authentication/controller.js
+++ api/authentication/controller.js
@@ -38,7 +38,7 @@ exports.login = async (req, res) => {
 
     // If isMobile is "true", check the version number
     if (isMobile === "true") {
-      if (versionNo !== "1.0.4") {
+      if (versionNo !== "1.0.5") {
         return errReturned(res, "Please update your mobile app.");
       }
     }
