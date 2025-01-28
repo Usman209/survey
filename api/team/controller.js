bc36e56ffbb3e03198d2feef5dc9b103edda4638
Usman Jamil <UJamil@lmkr.com>
2025-01-28 12:31:02 +0500

    code

--- api/team/controller.js
+++ api/team/controller.js
@@ -76,9 +76,9 @@ exports.createTeam = async (req, res) => {
     const { teamName, siteType, location, flws } = req.body;
 
     // Check if siteType and location are provided
-    if (!siteType || !location) {
-      return errReturned(res, "Site type and location are required.");
-    }
+    // if (!siteType || !location) {
+    //   return errReturned(res, "Site type and location are required.");
+    // }
 
     // If no teamName is provided and siteType is not selected, generate a unique team name
     let generatedTeamName = teamName || await generateUniqueTeamName(uc);
